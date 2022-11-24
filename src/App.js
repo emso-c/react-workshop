ffd61072e5f181136e74af6ec9fa28a4ff238420
Emir <emirsoyalan79@gmail.com>
2022-11-24 13:40:35 +0300

    Add up and down buttons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button />
+        <Button up />
+        <span style={{paddingTop: 20}}></span>
+        <Button down />
       </header>
     </div>
   );
