841ae6348a1ad7dcb9c16ddde30453c49e08446b
Emir <emirsoyalan79@gmail.com>
2022-11-24 16:13:10 +0300

    Add prettier package

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,30 +1,40 @@
-import React, { useState } from 'react'
-import Button from '@components/atoms/Button';
-import Text from '@components/atoms/Text';
+import Button from "@components/atoms/Button";
+import Text from "@components/atoms/Text";
+import React, { useState } from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0);
-    return (
-        <>
-            <Text Text="Sabır Counter" />
-            <div style={styles.container}>
-                <Button down onClick={()=>{setCount(count-1)}}/>
-                <Text Text={count}/>
-                <Button up onClick={()=>{setCount(count+1)}}/>
-            </div>
-            <Text Text={count<0 ? "Sabrım kalmadı": "Sabırlıyım"} />
-        </>
-    )
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Text Text="Sabır Counter" />
+      <div style={styles.container}>
+        <Button
+          down
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        />
+        <Text Text={count} />
+        <Button
+          up
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        />
+      </div>
+      <Text Text={count < 0 ? "Sabrım kalmadı" : "Sabırlıyım"} />
+    </>
+  );
 }
 
 const styles = {
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '20vh',
-        gap: '2rem'
-    }
-}
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "20vh",
+    gap: "2rem",
+  },
+};
 
-export default Counter
+export default Counter;
