b96bfdeaffd2c8a107e2fe0dfca7608843f3bacc
Emir <emirsoyalan79@gmail.com>
2022-11-24 15:05:38 +0300

    Add Counter to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/atoms/Button';
+import Counter from '@features/counter/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button up />
-        <span style={{paddingTop: 20}}></span>
-        <Button down />
+        <Counter />
       </header>
     </div>
   );
