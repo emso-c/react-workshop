ae2f7ce24e50fa0e61609cc6260f5b81ecc00875
Emir <emirsoyalan79@gmail.com>
2022-11-24 13:25:13 +0300

    Add Button component

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = () => {
+  return (
+    <button className="button">
+        <span>arttır</span>
+    </button>
+  )
+}
+
+export default Button
