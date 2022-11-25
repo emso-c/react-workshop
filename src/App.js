06a9b1d1ddf292f423863f7a89e67a56ef9f019c
Emir <emirsoyalan79@gmail.com>
2022-11-25 10:50:18 +0300

    Add Posts to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // @ts-ignore
 import Counter from '@features/counter/Counter'
+// @ts-ignore
+import Posts from '@features/posts/Posts'
 import './App.css';
 import React from 'react';
 
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Counter />
+        <Posts />
       </header>
     </div>
   );
