a423e821b3b06a1da5991306f2b8b219f5e892db
Emir <emirsoyalan79@gmail.com>
2022-11-25 10:54:59 +0300

    Remove app header

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Counter />
         <Posts />
-      </header>
     </div>
   );
 }
