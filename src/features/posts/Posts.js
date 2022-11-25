1c549f8e4cb2602da6a79cfdafa9145e1b41d3c0
Emir <emirsoyalan79@gmail.com>
2022-11-25 11:17:02 +0300

    Fetch posts from json-server

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3004/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data))
+    }, [])
+  return posts.map(post => (
+        <div key={post.id}>
+            <h2>{post.title}</h2>
+            <img width={300} height={300} src={post.image}></img>
+            <h6>{post.author}</h6>
+            <p>{post.content}</p>
+        </div>
+    ))
 }
 
 export default Posts
