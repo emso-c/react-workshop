28129a379d2ce99705d1cd74932fe4934093231d
Emir <emirsoyalan79@gmail.com>
2022-11-25 13:45:35 +0300

    Redesign posts view

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -8,13 +8,37 @@ const Posts = () => {
         .then(data => setPosts(data))
     }, [])
   return posts.map(post => (
-        <div key={post.id}>
-            <h2>{post.title}</h2>
-            <img width={300} height={300} src={post.image}></img>
-            <h6>{post.author}</h6>
-            <p>{post.content}</p>
+        <div key={post.id} 
+        // @ts-ignore
+        style={styles.container}>
+            <img style={styles.image} alt={post.title} src={post.image}></img>
+            <div 
+            // @ts-ignore
+            style={styles.textsContainer}>
+                <h2>{post.title}</h2>
+                {/* <h6>{post.author}</h6> */}
+                <p>{post.content}</p>
+            </div>
         </div>
     ))
+}
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 10,
+    },
+    textsContainer: {
+        fisplay: 'flex',
+        flexDirection: 'column',
+        margin: 10,
+        gap: 10
+    },
+    image: {
+        width: 100,
+        height: 150
+    }
 }
 
 export default Posts
